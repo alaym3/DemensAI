f1e40aa8bfce8ed98223e5cd4345838544df400a
alaym3 <aam5xa@virginia.edu>
2022-10-25 17:58:37 +0200

    + css

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>DEMENSAI</h1>
+      </header>
+      <div className="recording">
+        <h3>h3 test</h3>
         <h2>It's me. Hi. I'm the problem, it's me.</h2>
         <p>insert other T swift quote</p>
-      </header>
-
+      </div>
     </div>
+    
+
   );
 }
 
